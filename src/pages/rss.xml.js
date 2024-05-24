738228a530b790eeeaada464fab263554a1abe32
Ankur Dutta <ankur.611@gmail.com>
2024-05-24 22:07:08 +0530

    Rss configured and styles fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'bloodred17.dev - blog',
-    description: 'A fun blog featuring my experiments with anything tech',
+    title: 'bloodred17.dev - Blood\'s Tinkering Lab',
+    description: 'A fun blog featuring my experiments with tech',
     site: context.site,
     items: await pagesGlobToRssItems(
       import.meta.glob('./posts/*.{md,mdx}'),
